2ae72831b8272eaef3166c27cbf40d366e9709cb
Nguyen Huu Phuoc <Hailua5gs@gmail.com>
2015-10-10 14:43:34 +0700

    asdssdf

--- src/controller/IndexController.java
+++ src/controller/IndexController.java
@@ -1,13 +1,47 @@
 package controller;
 
+import java.util.Calendar;
+import java.util.Date;
+
 import javax.faces.bean.*;
 
-@ManagedBean(name="indexController")
+@ManagedBean(name = "indexController")
 @SessionScoped
 public class IndexController {
-	
+	private Date toDay = Calendar.getInstance().getTime();
+
+	public Date getToDay() {
+		return toDay;
+	}
+
+	public void setToDay(Date toDay) {
+		this.toDay = toDay;
+	}
+
+	private Date ngayNhan;
+	private Date ngayTra;
+
+	public Date getNgayNhan() {
+		return ngayNhan;
+	}
+
+	public void setNgayNhan(Date ngayNhan) {
+		this.ngayNhan = ngayNhan;
+	}
+
+	public Date getNgayTra() {
+		return ngayTra;
+	}
+
+	public void setNgayTra(Date ngayTra) {
+		this.ngayTra = ngayTra;
+	}
+
 	public String linkIndex() {
 		return "index?faces-redirect=true";
 	}
 
+	public String linkAdmin() {
+		return "admin?faces-redirect=true";
+	}
 }
